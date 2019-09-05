30d0012554a1737309a96f7de7a746bf3fe776c3
Ian Richard <ianrichard03@gmail.com>
2019-09-04 22:51:42 -0500

    moved assets to view folder.

--- views/assets/js/tracker.js
+++ views/assets/js/tracker.js
@@ -1,5 +1,7 @@
 $(document).ready( function(){
-    
+    alert("Jquery loaded!");
+   
+    /**
     let game = {
         phase : {
             name: ['Setup', 'Hero', 'Movement', 'Charge', 'Combat', 'Battleshock'],
@@ -10,10 +12,7 @@ $(document).ready( function(){
             'Take and Hold', 'Knife to the Heart', 'Total Conquest', 'Duality of Death', 'Battle for the Pass',
             'Starstrike', 'Scorched Earth', 'Total Commitment', 'Focal Points', 'The Better Part of Valuour',
             'Shifting Objectives', 'Places of Arcane Power', 'The Relocation Orb'],
-        players: []
+        players: [],
     };
-
-    $('#selectedBattle').text(game.battle[0]);
-    $('.dropdown-trigger').dropdown();
-    
+    */
 });
